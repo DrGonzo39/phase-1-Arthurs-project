d76c10a3e128613550329335d65ad7bea2a412dd
Arthur Benware <artpolitic91@gmail.com>
2022-10-28 20:30:47 -0400

    began first function

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetch("http://localhost:3000/people")
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            data.filter((person) => {
+                console.log(person.name)
+            })
+        })
 })

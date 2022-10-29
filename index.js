dbcf205b7c4d93be8401e7a53963b5c028214d4a
Arthur Benware <artpolitic91@gmail.com>
2022-10-28 20:01:22 -0400

    settled on DOMContentLoaded

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-document.addEventListener('load', () => {
-    fetch('http://localhost:3000/people')
+document.addEventListener('DOMContentLoaded', () => {
+    fetch("http://localhost:3000/people")
         .then(response => response.json())
         .then(data => console.log(data))
 })

b78490721a5e41356113d24c498590312cdeb9b0
Arthur Benware <artpolitic91@gmail.com>
2022-10-31 13:30:51 -0400

    started second function

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(response => response.json())
         .then(data => {
             data.filter((person) => {
-                console.log(person.name)
                 const li = document.createElement('li');
                 const ul = document.querySelector('ul');
                 li.textContent = person.name;
                 ul.appendChild(li);
+                
+                li.addEventListener('click', (event) => {
+                    console.log(event)
+                })
             })
         })
 })
